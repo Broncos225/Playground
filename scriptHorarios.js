88cd64a26eb4fc4dad4caad663bf0991d61e165c
Heracles <102176786+Broncos225@users.noreply.github.com>
2024-01-31 12:58:16 -0500

    Ingresode la tabla descansos

--- scriptHorarios.js
+++ scriptHorarios.js
@@ -15,6 +15,7 @@ const db = firebase.database();
 window.onload = function () {
     colorCelda();
     cargarDatos();
+    
 };
 
 function colorCelda() {
@@ -29,43 +30,54 @@ function colorCelda() {
 }
 
 function actualizarColorCelda(celda) {
+    var colorT1 = '#FFB31F';
+    var colorT2 = '#ffe699';
+    var colorT3 = '#c9c9c9';
+    var colorT4 = '#f4b084';
+    var colorT5 = '#aad18f';
+    var colorT6 = '#00ff00';
+    var colorTSA = '#ffff00';
+    var colorAM = 'cyan';
+    var colorD = '#white';
+
+
     const texto = celda.textContent.trim();
     let color;
     switch (texto) {
         case 'T1':
         case 'T1R1':
-            color = '#ddebf7';
+            color = colorT1;
             break;
         case 'T2':
         case 'T2R1':
-            color = '#ffe699';
+            color = colorT2;
             break;
         case 'T3':
         case 'T3R1':
-            color = '#c9c9c9';
+            color = colorT3;
             break;
         case 'T4':
         case 'T4R1':
-            color = '#f4b084';
+            color =  colorT4;
             break;
         case 'T5':
         case 'T5R1':
-            color = '#aad18f';
+            color = colorT5;
             break;
         case 'T6':
         case 'T6R1':
-            color = '#00ff00';
+            color = colorT6;
             break;
         case 'D':
         case 'DF':
-            color = '#white';
+            color = colorD;
             celda.style.color = 'red';
             break;
         case 'TSA':
-            color = '#ffff00';
+            color = colorTSA;
             break;
         case 'AM':
-            color = 'cyan';
+            color = colorAM;
             break;
     }
     celda.style.backgroundColor = color;
@@ -105,6 +117,7 @@ function cargarDatos() {
                     celda.textContent = data.texto;
                     actualizarColorCelda(celda);
                 }
+                contDescansos(); // Call the function here
             })
             .catch(error => {
                 console.error("Error al cargar datos:", error);
@@ -114,4 +127,55 @@ function cargarDatos() {
 
 document.getElementById('btnGuardar').addEventListener('click', guardarCeldas);
 
+function contDescansos(){
+    var contA = 0, contB = 0, contC = 0, contD = 0, contE = 0, contF = 0;
 
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('A' + i);
+        if (celda.textContent == 'D') {
+            contA += 1;
+        }
+    }
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('B' + i);
+        if (celda.textContent == 'D') {
+            contB += 1;
+        }
+    }
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('C' + i);
+        if (celda.textContent == 'D') {
+            contC += 1;
+        }
+    }
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('D' + i);
+        if (celda.textContent == 'D') {
+            contD += 1;
+        }
+    }
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('E' + i);
+        if (celda.textContent == 'D') {
+            contE += 1;
+        }
+    }
+    for (var i=1; i < 30; i++) {
+        var celda = document.getElementById('F' + i);
+        if (celda.textContent == 'D') {
+            contF += 1;
+        }
+    }
+    var celdaA = document.getElementById("1");
+    celdaA.textContent = contA;
+    var celdaB = document.getElementById("2");
+    celdaB.textContent = contB;
+    var celdaC = document.getElementById("3");
+    celdaC.textContent = contC;
+    var celdaD = document.getElementById("4");
+    celdaD.textContent = contD;
+    var celdaE = document.getElementById("5");
+    celdaE.textContent = contE;
+    var celdaF = document.getElementById("6");
+    celdaF.textContent = contF;
+}
