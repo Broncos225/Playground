0a8c70eeed1177e5afc0cb2743303b9aec6c9d2d
Heracles <102176786+Broncos225@users.noreply.github.com>
2024-02-05 14:48:26 -0500

    Exportar v1

--- scriptHorarios.js
+++ scriptHorarios.js
@@ -189,5 +189,37 @@ function resaltarDiaActual() {
     celda.cells[dia].style.color = "black";
 }
 
+function exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+    
+    // Specify file name
+    filename = filename?filename+'.xls':'excel_data.xls';
+    
+    // Create download link element
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+}
 
-
+document.getElementById('btnExportar').addEventListener('click', function() {
+    exportTableToExcel('Table', 'Horarios');
+});
