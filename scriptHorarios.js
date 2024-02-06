1e89f1d72cf911909d80c2b4b9fe5ed8d37aa2b3
Heracles <102176786+Broncos225@users.noreply.github.com>
2024-02-06 08:39:01 -0500

    Act Exportar a Excel Sin color

--- scriptHorarios.js
+++ scriptHorarios.js
@@ -189,39 +189,21 @@ function resaltarDiaActual() {
     celda.cells[dia].style.color = "black";
 }
 
-function exportTableToExcel(tableID, filename = ''){
-    var downloadLink;
-    var dataType = 'application/vnd.ms-excel';
-    var tableSelect = document.getElementById(tableID);
-    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-    
-    // Specify file name
-    filename = filename?filename+'.xls':'excel_data.xls';
-    
-    // Create download link element
-    downloadLink = document.createElement("a");
-    
-    document.body.appendChild(downloadLink);
-    
-    if(navigator.msSaveOrOpenBlob){
-        var blob = new Blob(['ufeff', tableHTML], {
-            type: dataType
-        });
-        navigator.msSaveOrOpenBlob( blob, filename);
-    }else{
-        // Create a link to the file
-        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-    
-        // Setting the file name
-        downloadLink.download = filename;
-        
-        //triggering the function
-        downloadLink.click();
+
+
+
+function exportarExcel() {
+    var table = document.getElementById("Table");
+    var wb = XLSX.utils.table_to_book(table);
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
     }
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), "Horarios.xlsx");
 }
 
-document.getElementById('btnExportar').addEventListener('click', function() {
-    exportTableToExcel('Table', 'Horarios');
-});
-
-
+document.getElementById('btnExportar').addEventListener('click', exportarExcel);
