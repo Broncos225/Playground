1bcd118ae7c1a758b56d05b168d638024a749044
Heracles <102176786+Broncos225@users.noreply.github.com>
2024-02-07 20:56:07 -0500

    Act Solicitudes

--- scriptHorarios.js
+++ scriptHorarios.js
@@ -207,3 +207,36 @@ function exportarExcel() {
 }
 
 document.getElementById('btnExportar').addEventListener('click', exportarExcel);
+
+let agentes = {
+    Anderson_Cano_Londoño: {
+        nombre: "Anderson",
+        correo: "anderson.cano@arus.com.co",
+        letra: "A"
+    },
+    Miguel_Cadavid_Naranjo: {
+        nombre: "Miguel",
+        correo: "miguel.cadavid@arus.com.co",
+        letra: "B"
+    },
+    Milton_Alexis_Calle_Londoño: {
+        nombre: "Milton Alexis",
+        correo: "milton.calle@arus.com.co",
+        letra: "C"
+    },
+    Yesica_Johana_Cano_Quintero: {
+        nombre: "Yesica Johana",
+        correo: "yesica.cano@arus.com.co",
+        letra: "D"
+    },
+    Andrés_Felipe_Vidal_Medina: {
+        nombre: "Andrés Felipe",
+        correo: "andres.vidal@arus.com.co",
+        letra: "E"
+    },
+    Andrés_Felipe_Yepes_Tascón: {
+        nombre: "Andrés Felipe",
+        correo: "andres.yepes@arus.com.co",
+        letra: "F"
+    }
+}
