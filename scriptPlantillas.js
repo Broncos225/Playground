e53af143edee058a9ef3c206f6c2a56a304e3a31
Andrés Yepes <102176786+Broncos225@users.noreply.github.com>
2025-10-16 13:55:36 -0500

    Update scriptPlantillas.js

--- scriptPlantillas.js
+++ scriptPlantillas.js
@@ -854,7 +854,7 @@ async function copiarTexto(id) {
             }
         }, 15000);
     }
-    if (window.innerWidth <= 300 && id === 'textoC') {
+    if (window.innerWidth <= 370 && id === 'textoC') {
         if (typeof window.closeModal === 'function') window.closeModal();
         else console.error('closeModal no está definida');
     }
