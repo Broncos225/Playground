85e95de84ef3f985dd9a76baa5c88c750770406b
Heracles <andres.yepes419@pascualbravo.edu.co>
2025-10-21 23:47:33 -0500

    Update Notificador.js

--- Notificador.js
+++ Notificador.js
@@ -1,3 +1,29 @@
+
+function CuentaAsesor() {
+    var nombre = localStorage.getItem('nombreAsesorActual');
+    var asesor = document.getElementById("AsesorActual");
+    var span = document.createElement("span");
+
+    if (nombre) {
+        nombre = nombre.replace(/_/g, ' ');
+        asesor.textContent = "Bienvenido/a ";
+        span.textContent = nombre;
+    } else {
+        asesor.textContent = "Bienvenido/a ";
+        span.textContent = "Nadie";
+    }
+
+    span.style.fontWeight = "lighter";
+    asesor.appendChild(span);
+}
+
+function seleccionarNombre(nombre) {
+    localStorage.setItem('nombreAsesorActual', nombre);
+    cerrarModal2();
+    CuentaAsesor();
+
+}
+
 // ============================================
 // CONFIGURACIÓN DE LA API
 // ============================================
